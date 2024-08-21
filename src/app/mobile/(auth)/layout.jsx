99d16b73b16d3047dd7d91becc244a79552f5300
varun606netzoptimize <varun@netzoptimize.com>
2024-08-21 19:55:49 +0530

    added border to mobile layout

--- src/app/mobile/(auth)/layout.jsx
+++ src/app/mobile/(auth)/layout.jsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }) => {
   return (
-    <div className='h-full w-full max-w-[430px] mx-auto'>
+    <div className='h-full w-full max-w-[430px] mx-auto' style={{ border: '1px solid #cdcdcd' }}>
       <div className='flex flex-col justify-between h-full text-left'>
         <div className='flex items-center justify-center mt-20'>
           <img src='/images/mobile/appLogo.png' alt='App Logo' width={220} height={230} className='object-contain' />
--- src/app/mobile/(main)/layout.jsx
+++ src/app/mobile/(main)/layout.jsx
@@ -2,7 +2,7 @@ import MobileFooter from '@/components/MobileFooter/MobileFooter'
 
 const mainLayout = ({ children }) => {
   return (
-    <div className='h-full flex flex-col w-full max-w-[430px] mx-auto'>
+    <div className='h-full flex flex-col w-full max-w-[430px] mx-auto' style={{ border: '1px solid #cdcdcd' }}>
       <div className='h-full'>{children}</div>
       <MobileFooter />
     </div>
