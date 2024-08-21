9f0beba552246fbf5709141f7e3aacee796cee77
varun606netzoptimize <varun@netzoptimize.com>
2024-08-21 19:25:43 +0530

    fixing font error

--- src/app/mobile/layout.jsx
+++ src/app/mobile/layout.jsx
@@ -3,10 +3,9 @@ import localFont from 'next/font/local'
 import { clsx } from 'clsx'
 
 // Font files can be colocated inside of `pages`
-const myFont = localFont({ src: './AvertaRegular.woff2' })
 
 const MobileLayout = ({ children }) => {
-  return <div className={clsx(myFont.className, 'h-full')}>{children}</div>
+  return <div className={clsx('h-full')}>{children}</div>
 }
 
 export default MobileLayout
