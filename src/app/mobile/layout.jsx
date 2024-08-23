2465d512682836bf877be54112798f40afc8fef2
yash-netzoptimize <126166539+yash-netzoptimize@users.noreply.github.com>
2024-08-23 16:05:51 +0530

    Update layout.jsx

--- src/app/mobile/layout.jsx
+++ src/app/mobile/layout.jsx
@@ -5,7 +5,7 @@ import { clsx } from 'clsx'
 // Font files can be colocated inside of `pages`
 
 const MobileLayout = ({ children }) => {
-  return <div className={clsx('h-screen')}>{children}</div>
+  return <div className={clsx('h-full')}>{children}</div>
 }
 
 export default MobileLayout
