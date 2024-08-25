8d2a28ca4805571701d2827ac3df06be9210528c
Yash Kumar <yash.netzoptimize@gmail.com>
2024-08-25 16:36:42 +0530

    Update TabletSideBar.jsx

--- src/components/TabletSideBar/TabletSideBar.jsx
+++ src/components/TabletSideBar/TabletSideBar.jsx
@@ -10,8 +10,8 @@ const menuItems = [
 const TabletSideBar = () => {
   return (
     <div className='w-28 bg-baseColor text-textColor p-4 sticky top-0 h-full'>
-      {menuItems.map(item => (
-        <Link href={item.href}>
+      {menuItems.map((item, index) => (
+        <Link key={index} href={item.href}>
           <p
             className={`block p-2 rounded-md transition-colors ${
               item.name === 'Beer'
