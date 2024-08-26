dfc3a32ddd718e68b8ad35ca8549d857559b9848
Yash Kumar <yash.netzoptimize@gmail.com>
2024-08-26 14:05:12 +0530

    Update page.jsx

--- src/app/mobile/(tablet)/(menu)/drinks/page.jsx
+++ src/app/mobile/(tablet)/(menu)/drinks/page.jsx
@@ -4,7 +4,7 @@ import { Carousel, CarouselContent, CarouselDots, CarouselItem } from '@/compone
 import { useRef } from 'react'
 
 const CoffeeCard = ({ name = 'beer', price = '100', color = 'white' }) => (
-  <div className='p-4 bg-white rounded-2xl shadow-xl'>
+  <div className='p-4 bg-white rounded-2xl shadow-xl cursor-pointer'>
     <div className={`w-16 h-16 rounded-full ${color} mx-auto mb-2`}>{/* Heart icon here */}</div>
     <p className='text-center font-semibold'>{name}</p>
     <p className='text-center text-gray-600'>${price}</p>
