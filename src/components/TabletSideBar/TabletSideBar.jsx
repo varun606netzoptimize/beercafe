b2e31137fa293631f9fd297a791b5d9e384ce3d0
Yash Kumar <yash.netzoptimize@gmail.com>
2024-08-26 12:51:07 +0530

    Update TabletSideBar.jsx

--- src/components/TabletSideBar/TabletSideBar.jsx
+++ src/components/TabletSideBar/TabletSideBar.jsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const menuItems = [
   { name: 'Drinks', href: '/mobile/drinks' },
@@ -8,12 +11,18 @@ const menuItems = [
 ]
 
 const TabletSideBar = () => {
+  const pathname = usePathname()
+
+  console.log(pathname)
+
   return (
-    <div className='w-fit pt-24 bg-baseColor text-textColor p-0 sticky top-0 h-full'>
+    <div className='w-fit pt-[82px] bg-baseColor text-textColor p-0 sticky top-0 h-full'>
       {menuItems.map((item, index) => (
         <Link key={index} href={item.href} className='group transition-all duration-300'>
-          <div className='flex flex-col items-center transition-all duration-300 px-4 py-6 hover:bg-gray-800 border-transparent border-l-2 rounded-xs group-hover:border-primary'>
-            <p>{item.name}</p>
+          <div
+            className={`flex flex-col transition-all duration-300 bg-baseColor text-sm items-center px-3 py-6  border-l-2 rounded-xs group-hover:bg-gray-800 ${pathname === item.href ? 'bg-gray-800 border-l-2 !border-primary text-primary ' : 'border-transparent'}`}
+          >
+            <p className={`transition-all duration-300 ${pathname === item.href ? 'text-primary' : ''}`}>{item.name}</p>
           </div>
         </Link>
       ))}
