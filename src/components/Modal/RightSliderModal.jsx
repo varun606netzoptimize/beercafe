0f87e834ee9bdd78224e0f6f2e8123611048bc85
Yash Kumar <yash.netzoptimize@gmail.com>
2024-08-27 18:36:04 +0530

    Update RightSliderModal.jsx

--- src/components/Modal/RightSliderModal.jsx
+++ src/components/Modal/RightSliderModal.jsx
@@ -2,20 +2,33 @@ import clsx from 'clsx'
 
 const RightSliderModal = ({ isOpen = false, onClose = () => {}, children }) => {
   return (
-    <div
-      className={clsx(
-        'fixed inset-y-0 right-0 w-[80%] rounded-[12px] rounded-r-md drop-shadow-2xl  max-w-md bg-white shadow-lg transform transition-transform duration-300 ease-in-out',
-        {
-          'translate-x-0': isOpen,
-          'translate-x-full': !isOpen
-        }
+    <>
+      {/* Background Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-20"
+          onClick={onClose}
+        />
       )}
-    >
-      <button className='absolute text-black text-lg z-30 bg-transparent top-4 right-4' onClick={onClose}>
-        ✕
-      </button>
-      {children}
-    </div>
+
+      <div
+        className={clsx(
+          'fixed inset-y-0 right-0 w-[80%] max-w-md bg-white shadow-lg z-30 rounded-[12px] drop-shadow-2xl transform transition-transform duration-300 ease-in-out',
+          {
+            'translate-x-0': isOpen,
+            'translate-x-full': !isOpen
+          }
+        )}
+      >
+        <button
+          className='absolute text-black text-lg z-40 bg-transparent top-4 right-4'
+          onClick={onClose}
+        >
+          ✕
+        </button>
+        {children}
+      </div>
+    </>
   )
 }
 
