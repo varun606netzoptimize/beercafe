5fcdeb844fec75bfc04cd0a0106a63ca9b13c20d
Yash Kumar <yash.netzoptimize@gmail.com>
2024-08-28 11:39:26 +0530

    Update page.jsx

--- src/app/mobile/(tablet)/slider-page/page.jsx
+++ src/app/mobile/(tablet)/slider-page/page.jsx
@@ -8,8 +8,7 @@ import Link from 'next/link'
 
 import Autoplay from 'embla-carousel-autoplay'
 
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
-import { Card, CardContent } from '@/components/ui/card'
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
 import LeftArrow from '@/@menu/svg/LeftArrow'
 
 const sliderImages = [
@@ -40,60 +39,53 @@ const sliderImages = [
 ]
 
 const Page = () => {
-  const plugin = React.useRef(Autoplay({ delay: 2000, stopOnInteraction: false, stopOnLastSnap: false }));
+  const plugin = React.useRef(Autoplay({ delay: 2000, stopOnInteraction: false, stopOnLastSnap: false }))
 
   return (
-    <div className='h-full min-[800px]:h-screen'>
-      <Card className='border-0 h-full'>
-        <CardContent className='flex aspect-square items-center p-0 justify-center relative h-full w-full'>
-          <div>
-            <div className='absolute top-0 left-0 h-[500px] w-[500px] bg-[#fff] rounded-full translate-x-[-50%] translate-y-[-50%] z-20 drop-shadow-2xl'></div>
-            <Image
-              src='/images/mobile/appLogo.png'
-              className='absolute z-30 top-8 left-10'
-              alt='Beer Icon'
-              width={120}
-              height={130}
-            />
-          </div>
-          <Carousel
-            opts={{
-              loop: true
-            }}
-            plugins={[plugin.current]}
-            className='w-full max-w-full h-full'
-          >
-            <CarouselContent className='h-full'>
-              {sliderImages.map((image, index) => (
-                <CarouselItem key={index}>
-                  <div className='w-full h-full'>
-                    <Image
-                      src={image.src}
-                      alt={image.alt}
-                      className='object-cover w-full h-full'
-                      width={1024}
-                      height={1024}
-                    />
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            {/* <CarouselPrevious /> */}
-            {/* <CarouselNext /> */}
-          </Carousel>
-          <div>
-            {/* <div className='absolute bottom-0 right-0 h-[300px] w-[300px] bg-[#FFC85D] rounded-full translate-x-[50%] translate-y-[50%] z-20'></div> */}
-            <Link href='/mobile/drinks'>
-              <div className='absolute bottom-8 right-8 z-20 bg-posPrimaryColor rounded-posButtonRadius py-3 px-5 cursor-pointer text-white drop-shadow-lg hover:drop-shadow-2xl'>
-                <div className='flex items-center gap-2'>
-                  <p>Order Now</p>
-                  <LeftArrow className='w-6 h-6' />
-                </div>
+    <div className='h-full relative min-[800px]:h-screen'>
+      <div>
+        <div className='absolute top-0 left-0 h-[500px] w-[500px] bg-[#fff] rounded-full translate-x-[-50%] translate-y-[-50%] z-20 drop-shadow-2xl'></div>
+        <Image
+          src='/images/mobile/appLogo.png'
+          className='absolute z-30 top-8 left-10'
+          alt='Beer Icon'
+          width={120}
+          height={130}
+        />
+      </div>
+      <Carousel
+        opts={{
+          loop: true
+        }}
+        plugins={[plugin.current]}
+        className='w-full max-w-full h-full'
+      >
+        <CarouselContent className='h-full'>
+          {sliderImages.map((image, index) => (
+            <CarouselItem key={index}>
+              <div className='w-full h-full'>
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  className='object-cover w-full h-full'
+                  width={1024}
+                  height={1024}
+                />
               </div>
-            </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <div>
+        <Link href='/mobile/drinks'>
+          <div className='absolute bottom-8 right-8 z-20 bg-posPrimaryColor rounded-posButtonRadius py-3 px-5 cursor-pointer text-white drop-shadow-lg hover:drop-shadow-2xl'>
+            <div className='flex items-center gap-2'>
+              <p>Order Now</p>
+              <LeftArrow className='w-6 h-6' />
+            </div>
           </div>
-        </CardContent>
-      </Card>
+        </Link>
+      </div>
     </div>
   )
 }
