765420d6c5078186f16b009ffd11c406cb5e9819
yash-netzoptimize <yash.netzoptimize@gmail.com>
2024-09-02 18:23:22 +0530

    changes for fonts

--- src/app/tablet/(member)/layout.jsx
+++ src/app/tablet/(member)/layout.jsx
@@ -1,12 +1,10 @@
-import { Inter } from 'next/font/google'
 
 import clsx from 'clsx'
 
-const inter = Inter({ subsets: ['latin'] })
 
 const layout = ({ children }) => {
   return (
-    <div className={clsx('relative w-full h-full overflow-hidden', inter.className)}>
+    <div className={clsx('relative w-full h-full overflow-hidden')}>
       {/* <div className='absolute -top-[1060px] -left-[420px] w-[1220px] h-[1180px] bg-[#F9CA5B] opacity-100 rounded-full drop-shadow-md'></div> */}
       {children}
       {/* <div className='absolute -bottom-[1050px] -right-[580px] w-[1200px] h-[1200px] bg-[#F9CA5B] opacity-100 rounded-full drop-shadow-[2px_4px_6px_rgba(0,0,0,1)]  '></div> */}
--- src/app/tablet/layout.jsx
+++ src/app/tablet/layout.jsx
@@ -1,15 +1,20 @@
+import localFont from 'next/font/local'
+
 import { Inter } from 'next/font/google'
 
 import clsx from 'clsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const museoSlab = localFont({ src: '../../../public/fonts/MuseoSlab-700.woff2' })
+
+
 const TabletLayout = ({ children }) => {
   return (
     <div
       className={clsx(
         'max-w-[1024px] relative h-full w-full mx-auto border-r border-l border-[#cdcdcd]',
-        inter.className
+        museoSlab.className
       )}
     >
       {children}
