cf3637d387858204dd69b71714940e9c0f23102f
yash-netzoptimize <126166539+yash-netzoptimize@users.noreply.github.com>
2024-09-24 15:28:04 +0530

    Update page.jsx

--- src/app/(dashboard)/orders/page.jsx
+++ src/app/(dashboard)/orders/page.jsx
@@ -1,16 +1,20 @@
 'use client'
 
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useState } from 'react'
 
-import { redirect } from "next/navigation"
+import { redirect } from 'next/navigation'
 
-import axios from "axios"
+import axios from 'axios'
+
+import { Box, Button, CircularProgress, Typography } from '@mui/material'
+
+import { DataGrid } from '@mui/x-data-grid'
 
 import { AuthContext } from '@/context/AuthContext'
 import { ENDPOINT } from '@/endpoints'
 
 const Page = () => {
-  const { authToken, tokenCheck, setPageTitle, setOrders } = useContext(AuthContext)
+  const { authToken, tokenCheck, setPageTitle, setOrders, orders } = useContext(AuthContext)
   const [isLoading, setIsLoading] = useState(false)
   const [isTableRendering, setIsTableRendering] = useState(true)
 
@@ -19,7 +23,7 @@ const Page = () => {
       if (!authToken.token) {
         redirect('/login')
       } else {
-        getOrders();
+        getOrders()
       }
 
       setPageTitle('Orders')
@@ -38,7 +42,7 @@ const Page = () => {
         }
       })
       .then(res => {
-        console.log(res.data, "orders")
+        console.log(res.data, 'orders')
         setOrders(res.data)
       })
       .catch(err => {
@@ -50,10 +54,132 @@ const Page = () => {
       })
   }
 
-  return (
-<>
+  const columns = [
+    // {
+    //   field: 'id',
+    //   headerName: 'Order ID',
+    //   flex: 1,
+    //   renderCell: (params) => (
+    //     <Box>
+    //       <Typography>
+    //         <strong>{params?.row?.id}</strong>
+    //       </Typography>
+    //     </Box>
+    //   )
+    // },
+    {
+      field: 'customer',
+      headerName: 'Customer Name',
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <p>
+            <strong>
+              {params?.row?.Customer?.firstname} {params?.row?.Customer?.lastname}
+            </strong>
+          </p>
+        </Box>
+      )
+    },
+    {
+      field: 'cafe',
+      headerName: 'Cafe',
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <Typography>
+            <strong>
+              <small>{params?.row?.Cafe.name}</small>
+            </strong>
+          </Typography>
+          <Typography>
+            <small>{params?.row?.Cafe.address}</small>
+          </Typography>
+        </Box>
+      )
+    },
+    {
+      field: 'amount',
+      headerName: 'Amount',
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <p >
+            <strong>${params?.row?.amount.toFixed(2)}</strong>
+          </p>
+        </Box>
+      )
+    },
+    {
+      field: 'paymentMode',
+      headerName: 'Payment Mode',
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <p>
+            <strong>{params?.row?.paymentMode}</strong>
+          </p>
+        </Box>
+      )
+    },
+    {
+      field: 'paymentStatus',
+      headerName: 'Payment Status',
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <p>
+            <strong>{params?.row?.paymentStatus}</strong>
+          </p>
+        </Box>
+      )
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Created At',
+      flex: 1,
+      renderCell: (params) => (
+        <Box>
+          <p>
+            {new Date(params?.row?.createdAt).toLocaleString()}
+          </p>
+        </Box>
+      )
+    },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      flex: 1,
+      renderCell: params => (
+        <Box>
+          <Button variant='outlined' color='info' size='small' sx={{ marginRight: 2 }} onClick={() => {}}>
+            Details
+          </Button>
+        </Box>
+      ),
+      sortable: false
+    }
+  ];
 
-</>
+
+  return (
+    <div className='flex flex-col gap-6'>
+      {isTableRendering ? (
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <CircularProgress size={32} />
+        </Box>
+      ) : (
+        <DataGrid
+          loading={isLoading}
+          rows={orders}
+          columns={columns}
+          pagination
+          rowCount={orders?.length}
+
+          // getRowHeight={() => 'auto'}
+        />
+      )}
+    </div>
   )
 }
 
